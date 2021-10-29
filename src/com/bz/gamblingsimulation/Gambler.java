bff73113128870b52ad6ce05a53022a10b5fa536
Subhamdas20 <subham21.sd@gmail.com>
2021-10-29 23:36:52 +0530

    Added : uc2 start bet

---
+++ src/com/bz/gamblingsimulation/Gambler.java
@@ -0,0 +1,24 @@
+package com.bz.gamblingsimulation;
+
+public class Gambler {
+    /*
+     * Creating a gamblerSimulator
+     * Created a method startGambling to store initial capital and risk per game
+     * */
+    public void startGambling() {
+        int startingAmount = 100;
+        int bet_Per_Game = 1;
+
+        int bet_result = (int) (Math.random() * 10) % 2;
+        if (bet_result == 0){
+            startingAmount-=1;
+        }
+        else startingAmount+=1;
+
+    }
+
+    public static void main(String[] args) {
+        Gambler play = new Gambler();
+        play.startGambling();
+    }
+}
