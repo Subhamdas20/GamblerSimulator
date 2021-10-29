77f732d060a350078d385cea7098ad9774bc8a6a
Subhamdas20 <subham21.sd@gmail.com>
2021-10-29 23:22:51 +0530

    Added : uc1 which contains risk amount and bet per game

---
+++ src/com/bz/gamblingsimulation/gambler.java
@@ -0,0 +1,12 @@
+package com.bz.gamblingsimulation;
+
+public class gambler {
+    /*
+     * Creating a gamblerSimulator
+     * Created a method startGambling to store initial capital and risk per game
+     * */
+    public void startGambling() {
+        int startingAmount = 100;
+        int bet_Per_Game = 1;
+    }
+}
